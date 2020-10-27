721a6535c37114528fbef18ad56397f877502a32
Shelden <shelden@berkeley.edu>
2020-10-26 21:53:41 -0700

    added paper id

--- src/processor.py
+++ src/processor.py
@@ -4,24 +4,25 @@ from pathlib import Path
 
 input_path = (
      Path.cwd()
+     /".."
      /"output"
 )
 
 paths = list(input_path.glob('*.json'))
-
-output_file = open('hashmap.json', 'w')
+print(paths)
+output_file = open('../hashmap.json', 'w')
 labels = {}
 
 for file in paths:
 	data = json.load(open(file))
 	print("Doing {}".format(file))
-	for paper in list(data.values()):
+	for paper_id, paper in data.items():
 		for sentence in paper:
 			for i in range(len(sentence[1])):
 				entity = sentence[1][i][0]
 				label = sentence[1][i][1]
 				if not entity in labels:
-					labels[entity] = label
+					labels[entity] = [label, paper_id]
 
 
 json.dump(labels, output_file)
